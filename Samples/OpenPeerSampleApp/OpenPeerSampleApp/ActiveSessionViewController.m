27655125ce1309382849cce825d859d7ed62c84d
Vladimir Morosev <mosa@hookflash.com>
2013-10-16 16:14:20 +0200

    - AEC and AGC processing are set to enabled status by default

--- Samples/OpenPeerSampleApp/OpenPeerSampleApp/ActiveSessionViewController.m
+++ Samples/OpenPeerSampleApp/OpenPeerSampleApp/ActiveSessionViewController.m
@@ -104,7 +104,11 @@
     [[HOPMediaEngine sharedInstance] setChannelRenderView:self.videoImageView];
     //Set default video orientation to be portrait
     [[HOPMediaEngine sharedInstance] setDefaultVideoOrientation:HOPMediaEngineVideoOrientationPortrait];
-    
+    // Set default settings for AEC, AGC and NS
+    [[HOPMediaEngine sharedInstance] setEcEnabled:YES];
+    [[HOPMediaEngine sharedInstance] setAgcEnabled:YES];
+    [[HOPMediaEngine sharedInstance] setNsEnabled:NO];
+  
     [self.view bringSubviewToFront:self.buttonsView];
     //Prepare view controller for default state - no call
     [self prepareForCall:NO withVideo:NO];
