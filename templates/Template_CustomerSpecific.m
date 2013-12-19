4864322324bd37223a9c9c4649e6b408f6144630
Robin Raymond <robin@hookflash.com>
2013-12-19 16:46:42 -0500

    made template file prettier

--- templates/Template_CustomerSpecific.m
+++ templates/Template_CustomerSpecific.m
@@ -33,37 +33,47 @@
 
 #error VALUES BELOW NEED TO BE SET TO SUPPORT CUSTOMER DEVELOPER ENVIRONMENT
 
-//!!!!!!!!!!!!!!!!!!!! WARNING!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! WARNING !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 //-----------------------------------------------------------------------------
 // If you have not done so already, please go to http://fly.hookflash.me to
 // create your application and obtain your app ID and shared secret.
 //-----------------------------------------------------------------------------
-NSString* const applicationId = @"<-- insert application ID here (e.g. com.domain.appName) -->";
-
-NSString* const applicationIdSharedSecret = @"<-- insert shared secret here -->";
-// ------------------- !!! WARNING !!! -------------------
 // Setting the application shared secret in the client is not recommended.
 // Instead, the recommended solution is for your client download an authorized
 // application ID from your own server using whatever method you determine is
 // most appropriate.
-// ------------------- !!! WARNING !!! -------------------
+//-----------------------------------------------------------------------------
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! WARNING !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-//Set customer spplication name
+//-----------------------------------------------------------------------------
+NSString* const applicationId = @"<-- insert application ID here (e.g. com.domain.appName) -->";
+
+NSString* const applicationIdSharedSecret = @"<-- insert shared secret here -->";
+//-----------------------------------------------------------------------------
+
+// set customer spplication name
 NSString* const applicationName = @"OpenPeerSampleApp";
-//Set customer spplication image url
-NSString* const applicationImageURL = @"http://hookflash.com/wp-content/themes/CleanSpace/images/logo.png";
-//Set customer spplication url
-NSString* const applicationURL = @"www.openpeer.org";
-//Set outer frame url
-NSString* const outerFrameURL = @"https://app-javascript.hookflash.me/outer.html?view=choose";
-//Set outer namespace grant url
-NSString* const namespaceGrantServiceURL = @"https://app-javascript.hookflash.me/outernamespacegrant.html";
-//Set identity provider domain
-NSString* const identityProviderDomain = @"idprovider-javascript.hookflash.me";
-//Set federated identity base
-NSString* const identityFederateBaseURI = @"identity://idprovider-javascript.hookflash.me/";
-//Set lockbox service domain
-NSString* const lockBoxServiceDomain =  @"hcs-javascript.hookflash.me";
-//Set default outgoing telent server
-NSString * const defaultOutgoingTelnetServer = @"tcp.logger.hookflash.me:8055";
 
+// set customer spplication image url
+NSString* const applicationImageURL = @"http://hookflash.com/wp-content/themes/CleanSpace/images/logo.png";
+
+// set customer spplication url
+NSString* const applicationURL = @"https://www.openpeer.org/";
+
+// set outer frame url
+NSString* const outerFrameURL = @"https://app-javascript.hookflash.me/outer.html?view=choose";
+
+// set outer namespace grant url
+NSString* const namespaceGrantServiceURL = @"https://app-javascript.hookflash.me/outernamespacegrant.html";
+
+// set identity provider domain
+NSString* const identityProviderDomain = @"idprovider-javascript.hookflash.me";
+
+// set federated identity base
+NSString* const identityFederateBaseURI = @"identity://idprovider-javascript.hookflash.me/";
+
+// set lockbox service domain
+NSString* const lockBoxServiceDomain =  @"hcs-javascript.hookflash.me";
+
+// set default outgoing telent server
+NSString * const defaultOutgoingTelnetServer = @"tcp.logger.hookflash.me:8055";
